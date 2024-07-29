2dc8c9495f97cb3ac8a7d07adf0d007ec3815f35
seoyeong <carsk0607@gmail.com>
2024-07-29 22:25:49 +0900

    chore: change directory structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,22 @@
 import 'package:flutter/material.dart';
-import 'following.dart';
-import 'explore.dart';
-import 'scrap.dart';
-import 'reels.dart';
-import 'myclip.dart';
+import 'screens/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'News Community',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,  // Number of tabs
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: const TabBarView(
-          children: [
-            Following(),
-            Explore(),
-            Scrap(),
-            Reels(),
-            Myclip(),
-          ],
-        ),
-        bottomNavigationBar: Material(
-          color: Colors.white, // Adjust the color as needed
-          child: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.people), text: 'Follwing'),
-              Tab(icon: Icon(Icons.explore), text: 'Explore'),
-              Tab(icon: Icon(Icons.create), text: 'Scrap'),
-              Tab(icon: Icon(Icons.video_library), text: 'Reels'),
-              Tab(icon: Icon(Icons.folder), text: 'Myclip'),
-            ],
-            indicatorSize: TabBarIndicatorSize.label, // Makes indicator full width of the tab
-          ),
-        ),
-      ),
+      home: Home(), // 로그인 화면을 첫 화면으로 설정
+      routes: {
+        
+      },
     );
   }
 }
