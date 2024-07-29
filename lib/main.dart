1a17b22a32f0db83b1e057eb6a65c7dff706cf15
seoyeong <carsk0607@gmail.com>
2024-07-30 00:27:10 +0900

    feat: add signup and signup_naver

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/login.dart';
 import 'screens/home.dart';
+import 'screens/signup.dart';
+import 'screens/signup_naver.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,6 +19,9 @@ class MyApp extends StatelessWidget {
       home: Login(), // 로그인 화면을 첫 화면으로 설정
       routes: {
         '/home': (context) => Home(), // 메인 화면을 위한 라우트 설정
+        '/login': (context) => Login(), // 로그인 화면을 위한 라우트 설정
+        '/signup': (context) => Signup(), // 회원가입 화면을 위한 라우트 설정
+        '/signupNaver': (context) => SignupNaver(), // 회원가입 화면을 위한 라우트 설정
       },
     );
   }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -13,16 +13,96 @@ class _LoginState extends State<Login> {
   void _login() {
     // 로그인 로직 구현 (API 호출 등)
     // 로그인 성공 시 메인 화면으로 이동
-    Navigator.pushReplacementNamed(context, '/home');
+    if (!_usernameController.text.isEmpty &&
+        !_passwordController.text.isEmpty) {
+      if (_usernameController.text == "test" &&
+          _passwordController.text == "1234") {
+        Navigator.pushReplacementNamed(context, '/home');
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('로그인 실패'),
+              content: Text('아이디 또는 비밀번호가 일치하지 않습니다.'),
+              actions: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.min, // 컬럼의 크기를 내용물 크기에 맞춤
+                  mainAxisAlignment: MainAxisAlignment.center, // 아이템들을 센터 정렬
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context); // 현재 대화 상자를 닫습니다.
+                      },
+                      child: Text('확인'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context); // 로그인 실패 대화 상자를 닫습니다.
+                        Navigator.pushNamed(
+                            context, '/signup'); // 회원가입 페이지로 이동합니다.
+                      },
+                      child: Text('회원가입'),
+                    ),
+                  ],
+                ),
+              ],
+              actionsAlignment: MainAxisAlignment.center, // 액션들을 가운데 정렬
+            );
+          },
+        );
+      }
+    }
   }
 
   void _naverLogin() async {
     try {
       final NaverLoginResult result = await FlutterNaverLogin.logIn();
-      final NaverAccountResult account = await FlutterNaverLogin.currentAccount();
+      final NaverAccountResult account =
+          await FlutterNaverLogin.currentAccount();
 
       print('로그인 성공: ${account}'); // 사용자 이메일 정보 출력
-      Navigator.pushReplacementNamed(context, '/home'); // 로그인 성공 시 홈 화면으로 이동
+      if (result.status == NaverLoginStatus.loggedIn) {
+        if (account.id == "JE3Nx5Ex9e2vRncn5c-JxXzY6hlVjCwM-GGqd6BZq1I") {
+          // 네이버 로그인 성공 시
+          print("go to home");
+          Navigator.pushReplacementNamed(context, '/home'); // 홈 화면으로 이동
+        } else {
+          print("fail to login");
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('로그인 실패'),
+                content: Text('아이디 또는 비밀번호가 일치하지 않습니다.'),
+                actions: <Widget>[
+                  Column(
+                    mainAxisSize: MainAxisSize.min, // 컬럼의 크기를 내용물 크기에 맞춤
+                    mainAxisAlignment: MainAxisAlignment.center, // 아이템들을 센터 정렬
+                    children: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context); // 현재 대화 상자를 닫습니다.
+                        },
+                        child: Text('확인'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context); // 로그인 실패 대화 상자를 닫습니다.
+                          Navigator.pushNamed(
+                              context, '/signupNaver'); // 회원가입 페이지로 이동합니다.
+                        },
+                        child: Text('회원가입'),
+                      ),
+                    ],
+                  ),
+                ],
+                actionsAlignment: MainAxisAlignment.center, // 액션들을 가운데 정렬
+              );
+            },
+          );
+        }
+      }
     } catch (e) {
       print('네이버 로그인 실패: $e');
     }
