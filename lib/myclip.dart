c4d924d07d8ff3c3da54c94c61e1ffe95812ee93
seoyeong <carsk0607@gmail.com>
2024-07-29 21:53:35 +0900

    first commit

---
+++ lib/myclip.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Myclip extends StatelessWidget {
+  const Myclip({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Myclip Tab'),
+    );
+  }
+}
---
+++ lib/scrap.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Scrap extends StatelessWidget {
+  const Scrap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Scrap Tab'),
+    );
+  }
+}
