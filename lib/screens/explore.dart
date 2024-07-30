894c4c17e50f55d4e78dcfc75955755584284d7a
haleyyi <hyoungjung.yi@gmail.com>
2024-07-31 01:33:10 +0900

    login + signup

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -4,23 +4,35 @@ import 'explore_sections/kingcrab_section.dart';
 import 'explore_sections/search_section.dart';
 
 class Explore extends StatelessWidget {
-  const Explore({super.key});
+  const Explore({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const SearchSection(), // 검색 섹션
-                const KingCrabSection(), // 킹크랩 섹션
-                const HotScrapSection(), // 핫스크랩 섹션
-              ],
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFFFFFFF), // 흰색
+              Color(0xFFD3E2FF), // 연파랄
+            ],
           ),
-        ],
+        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  SearchSection(), // 검색 섹션
+                  KingCrabSection(), // 킹크랩 섹션
+                  HotScrapSection(), // 핫스크랩 섹션
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
