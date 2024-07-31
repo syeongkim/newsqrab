3c83d42c5f2d9124b532145a115b07ab26f1a531
YoungMin0B <xoxoic00@gmail.com>
2024-08-01 04:26:18 +0900

    feature: kingcrab_king,follo,profi api 연결

--- lib/services/scrap_service.dart
+++ lib/services/scrap_service.dart
@@ -20,7 +20,6 @@ class ScrapService {
     }
   }
 
-  // 팔로우한 사용자의 스크랩 데이터를 가져오는 메서드 추가
   Future<List<dynamic>> fetchScrapsByFollowing(BuildContext context) async {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     final String url = '$baseUrl/following/${userProvider.userId}';
@@ -36,7 +35,6 @@ class ScrapService {
     }
   }
 
-  // 팔로워 이모지를 업데이트하는 메서드 추가
   Future<void> updateFollowerEmoji(String scrapId, String userId, String emoji) async {
     final String url = '$baseUrl/$scrapId/followerEmojis';
     final response = await http.put(
@@ -52,6 +50,50 @@ class ScrapService {
 
     if (response.statusCode != 200) {
       throw Exception('Failed to update emoji');
+    }
+  }
+
+  Future<List<dynamic>> fetchTopUsers() async {
+    final String url = 'http://175.106.98.197:3000/users/kings';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body) as List<dynamic>;
+    } else {
+      throw Exception('Failed to load top users');
+    }
+  }
+
+  // 팔로우 상태를 업데이트하는 메서드 추가
+  Future<void> updateFollowing(String userId, String followUserId) async {
+    final String url = 'http://175.106.98.197:3000/users/$userId/following/$followUserId';
+
+    final response = await http.put(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update following');
+    }
+  }
+
+  // 언팔로우 상태를 업데이트하는 메서드 추가
+  Future<void> deleteFollowing(String userId, String unfollowUserId) async {
+    final String url = 'http://175.106.98.197:3000/users/$userId/following/$unfollowUserId';
+
+    final response = await http.delete(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete following');
     }
   }
 }
