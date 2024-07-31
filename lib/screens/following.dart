7c891f8917186ba01d14537a4733bf0dd4091435
YoungMin0B <xoxoic00@gmail.com>
2024-08-01 08:27:20 +0900

    home 다이얼로그

--- lib/screens/following.dart
+++ lib/screens/following.dart
@@ -103,6 +103,152 @@ class _FollowingState extends State<Following> {
     }
   }
 
+  void _showPopup(BuildContext context, Map<String, dynamic> item) async {
+    List<dynamic> followingList = [];
+    List<dynamic> followerList = [];
+
+    try {
+      final user = await ScrapService().fetchUserById(item["userId"]);
+      followingList = user.following;
+      followerList = user.followers;
+    } catch (e) {
+      print('Failed to fetch user data: $e');
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(item['profileImage'] ?? 'assets/images/crabi.png'),
+              radius: 40,
+            ),
+            SizedBox(height: 8),
+            Text(
+              item['usernickname'] ?? 'Unknown',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Text(
+                          '팔로잉',
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          followingList.length.toString(),
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 32),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Text(
+                          '팔로워',
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          followerList.length.toString(),
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+          ],
+        ),
+        content: Container(
+          width: double.maxFinite,
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 8),
+                child: ListTile(
+                  title: Text(item['highlightedText'] ?? 'No Content'),
+                  subtitle: Text(item['createdAt'] ?? 'No Time'),
+                  onTap: () {
+                    _showDetailDialog(context, item);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Close'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _showDetailDialog(BuildContext context, Map<String, dynamic> item) async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Scrap Detail'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(item["title"] ?? 'No Content'),
+            SizedBox(height: 8),
+            Text(
+              item["createdAt"] ?? 'No Time',
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+            SizedBox(height: 8),
+            GestureDetector(
+              onTap: () async {
+                final url = item["url"];
+                if (url != null && await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: Text(
+                "원문보기",
+                style: TextStyle(fontSize: 12, color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Close'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -128,103 +274,108 @@ class _FollowingState extends State<Following> {
 
           return Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Card(
-              color: Colors.grey[300], // 카드 배경색을 회색으로 설정
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: profileImage.startsWith('http')
-                              ? NetworkImage(profileImage)
-                              : AssetImage(profileImage) as ImageProvider,
-                          radius: 20,
-                        ),
-                        SizedBox(width: 8),
-                        Text(
-                          profileName,
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8),
-                    Container(
-                      padding: EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.grey.shade300), // Optional: border color
+            child: GestureDetector(
+              onTap: () {
+                _showPopup(context, item);
+              },
+              child: Card(
+                color: Colors.grey[300], // 카드 배경색을 회색으로 설정
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: profileImage.startsWith('http')
+                                ? NetworkImage(profileImage)
+                                : AssetImage(profileImage) as ImageProvider,
+                            radius: 20,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            profileName,
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-                      child: Text(
-                        scrapContent,
-                        style: TextStyle(fontSize: 14),
+                      SizedBox(height: 8),
+                      Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.grey.shade300), // Optional: border color
+                        ),
+                        child: Text(
+                          scrapContent,
+                          style: TextStyle(fontSize: 14),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          scrapTime,
-                          style: TextStyle(fontSize: 12, color: Colors.grey),
-                        ),
-                        Icon(
-                          _getEmojiIcon(emoji),
-                          size: 14,
-                          color: Colors.grey,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8),
-                    GestureDetector(
-                      onTap: () async {
-                        if (link.isNotEmpty && await canLaunchUrl(Uri.parse(link))) {
-                          await launchUrl(Uri.parse(link));
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Could not launch $link')),
-                          );
-                        }
-                      },
-                      child: Text(
-                        '원문 링크 바로가기>>',
-                        style: TextStyle(fontSize: 12, color: Colors.blue),
+                      SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            scrapTime,
+                            style: TextStyle(fontSize: 12, color: Colors.grey),
+                          ),
+                          Icon(
+                            _getEmojiIcon(emoji),
+                            size: 14,
+                            color: Colors.grey,
+                          ),
+                        ],
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    // 이모티콘 섹션 추가
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.sentiment_very_satisfied),
-                          onPressed: () => _incrementEmoji(index, 0),
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        onTap: () async {
+                          if (link.isNotEmpty && await canLaunchUrl(Uri.parse(link))) {
+                            await launchUrl(Uri.parse(link));
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Could not launch $link')),
+                            );
+                          }
+                        },
+                        child: Text(
+                          '원문 링크 바로가기>>',
+                          style: TextStyle(fontSize: 12, color: Colors.blue),
                         ),
-                        Text('${emojiCounts[index][0]}'),
-                        SizedBox(width: 8),
-                        IconButton(
-                          icon: Icon(Icons.sentiment_satisfied),
-                          onPressed: () => _incrementEmoji(index, 1),
-                        ),
-                        Text('${emojiCounts[index][1]}'),
-                        SizedBox(width: 8),
-                        IconButton(
-                          icon: Icon(Icons.sentiment_dissatisfied),
-                          onPressed: () => _incrementEmoji(index, 2),
-                        ),
-                        Text('${emojiCounts[index][2]}'),
-                        SizedBox(width: 8),
-                        IconButton(
-                          icon: Icon(Icons.sentiment_very_dissatisfied),
-                          onPressed: () => _incrementEmoji(index, 3),
-                        ),
-                        Text('${emojiCounts[index][3]}'),
-                      ],
-                    ),
-                  ],
+                      ),
+                      SizedBox(height: 8),
+                      // 이모티콘 섹션 추가
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.sentiment_very_satisfied),
+                            onPressed: () => _incrementEmoji(index, 0),
+                          ),
+                          Text('${emojiCounts[index][0]}'),
+                          SizedBox(width: 8),
+                          IconButton(
+                            icon: Icon(Icons.sentiment_satisfied),
+                            onPressed: () => _incrementEmoji(index, 1),
+                          ),
+                          Text('${emojiCounts[index][1]}'),
+                          SizedBox(width: 8),
+                          IconButton(
+                            icon: Icon(Icons.sentiment_dissatisfied),
+                            onPressed: () => _incrementEmoji(index, 2),
+                          ),
+                          Text('${emojiCounts[index][2]}'),
+                          SizedBox(width: 8),
+                          IconButton(
+                            icon: Icon(Icons.sentiment_very_dissatisfied),
+                            onPressed: () => _incrementEmoji(index, 3),
+                          ),
+                          Text('${emojiCounts[index][3]}'),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -234,6 +385,3 @@ class _FollowingState extends State<Following> {
     );
   }
 }
-
-
-
